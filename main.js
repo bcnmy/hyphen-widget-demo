d9b1b604a960d75506b955c57937ed7a8331483d
Asheer Rizvi <asheer.vil@gmail.com>
2022-05-09 14:51:04 +0530

    feat: added a basic demo.

--- main.js
+++ main.js
@@ -1,33 +1,21 @@
 import "./style.css";
 import * as HyphenWidget from "@biconomy/hyphen-widget/dist";
-
-console.log(HyphenWidget)
+import "@biconomy/hyphen-widget/dist/index.css";
 
 const wid = HyphenWidget.default.init(document.getElementById("widget"), {
-  test: true, // if you're using testnet, it's required
+  test: false, // required if using testnet chains.
   apiKeys: {
-    // required
-    Fuji: "your.biconomy.api.key",
-    Goerli: "your.biconomy.api.key",
-    Mumbai: "your.biconomy.api.key",
-    Rinkeby: "your.biconomy.api.key",
+    // optional for gasless
+    Ethereum: "Ethereum API Key",
+    Polygon: "Polygon API Key",
+    Avalanche: "Avalanche API Key",
   },
   rpcUrls: {
     // required
-    Goerli: "https://your.eth.api.endpoint",
-    Mumbai: "https://your.eth.api.endpoint",
-    Rinkeby: "https://your.eth.api.endpoint",
+    Ethereum: "Ethereum RPC URL",
+    Polygon: "Polygon RPC URL",
+    Avalanche: "Avalanche RPC URL",
   },
-  // optional
-  lockSourceChain: true,
-  lockDestinationChain: true,
-  lockToken: true,
-  lockAmount: true,
-  defaultDestinationChain: "Mumbai",
-  defaultSourceChain: "Goerli",
-  defaultToken: "ETH",
-  defaultAmount: "0.01",
-  defaultReceiver: "0x000000000000000000000000000000000000dEaD",
   // NOTE: following 2 callback emit when tx is *sent*, you should check the status by yourself
   onDeposit: (e) => console.log("Deposit " + e), // emit when depost tx is sent
   onExit: (e) => console.log("Exit " + e), // emit when exit tx (receiver will receive tokens) is sent
@@ -40,6 +28,24 @@ const wid = HyphenWidget.default.init(document.getElementById("widget"), {
           receiver?: string;
           gasless: boolean;
       }
-     */
+  */
   onChange: (input) => console.log("Input " + JSON.stringify(input)),
 });
+
+// const wid = HyphenWidget.default.init(document.getElementById("widget"), {
+//   test: true, // required if using testnet chains.
+//   apiKeys: {
+//     // optional for gasless
+//     Fuji: "Fuji API Key",
+//     Goerli: "Goerli API Key",
+//     Mumbai: "Mumbai API Key",
+//   },
+//   rpcUrls: {
+//     // required
+//     Fuji: "Fuji RPC URL",
+//     Goerli: "Goerli RPC URL",
+//     Mumbai: "Mumbai RPC URL",
+//   },
+//   ...
+//   // Other options.
+// });
