e4a57e20f4f545b6f5d131b26d81858aa135e91a
Asheer Rizvi <asheer.vil@gmail.com>
2022-05-16 16:23:49 +0530

    feat: react demo.

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import * as HyphenWidget from "@biconomy/hyphen-widget";
+import "@biconomy/hyphen-widget/dist/index.css";
+
+function App() {
+  const [hyphenWidget, setHyphenWidget] = useState();
+
+  useEffect(() => {
+    const widget = HyphenWidget.default.init(
+      document.getElementById("widget"),
+      {
+        tag: "expecto-patronum",
+        showWidget: true,
+        showCloseButton: true,
+      }
+    );
+
+    if (widget) {
+      setHyphenWidget(widget);
+    }
+  }, []);
+
+  function handleOpen() {
+    hyphenWidget.open();
+  }
+
+  function handleClose() {
+    hyphenWidget.close();
+  }
+
+  return <div className="App">
+    <button onClick={handleOpen}>Open Widget</button>
+    <button onClick={handleClose}>Close Widget</button>
+
+    <div id="widget"></div>
+  </div>;
+}
+
+export default App;
